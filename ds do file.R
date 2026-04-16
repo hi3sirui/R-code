1d56ba2e424a84a83056a7e8573e20bd14283bb1
hi3sirui <hi.3.sirui@gmail.com>
2026-04-16 15:12:43 +0200

    Update ds do file.R

--- ds do file.R
+++ ds do file.R
@@ -4,24 +4,13 @@ ds <- read.csv("L:/Auditdata/Students/Lexi/Data_Lexi_v5.csv")
 View(ds)
 
 colnames(ds)
-
 #PREP----
-
-##identifying empty surveys----
-wave1 <- names(ds)[9:33]
-wave2 <- names(ds)[37:64]
+wave1 <- names(ds)[5:22]
+wave2 <- names(ds)[24:44]
 table(rowSums(is.na(ds[, wave1])))
 table(rowSums(is.na(ds[, wave2])))
 
-cat("Completely empty rows:", sum(rowSums(is.na(ds[, 9:ncol(ds)])) == (ncol(ds) - 8)), "\n")
-cat("All Wave 1 NA:", sum(rowSums(is.na(ds[, wave1])) == 25), "\n")
-cat("All Wave 2 NA:", sum(rowSums(is.na(ds[, wave2])) == 28), "\n")
-both_empty <- rowSums(is.na(ds[, wave1])) == 25 & rowSums(is.na(ds[, wave2])) == 28
-cat("Empty in both waves:", sum(both_empty), "\n")
 
-
-
-##renaming vars----
 ds <- ds %>%
   rename(LS21 = quality_of_life_a_k,
          H21 = height_k,
@@ -34,70 +23,55 @@ ds <- ds %>%
          WCT_21 = weight_change_k,
          momPhys_21 = physique_mom_k,
          dadPhys21 = physique_dad_k,
+         age_2021 = cpr_alder,
+         speUd_21  = work_b_k1,
+         diplUd_21 = work_b_k2,
+         #work: shift type
+         daySche_21 = work_schedule_a_k,
+         nightSche_21 = work_schedule_b_k,
+         eveSche_21 = work_schedule_c_k,
+         #no. of years working a shift type
+         dayScheYrs_21 = day_hours_k,
+         nightScheYrs_21 = evening_hours_k,
+         eveScheYrs_21 = night_hours_k,
          age_2024 = age,
          LS24 = qol,
          phyHealth_24 = phy_health_v2,
-         menHealth_24 = men_health_v2,
+         mentHealth_24 = men_health_v2,
          W24 = weight_k_v2,
          H24 = height_k_v2,
          WS_a24 = weight_statements_a_k_v2,
          WS_b24 = weight_statements_b_k_v2,
          WS_c24 = weight_statements_c_k_v2,
-         WS_d24 = weight_statements_a_k_v2,
-         WCT_24 = weight_change_k,
+         WS_d24 = weight_statements_d_k_v2,
+         WCT_24 = weight_change_k_v2,
          momPhys_24 = physique_mom_k_v2,
          dadPhys24 = physique_dad_k_v2,
-         age_2021 = cpr_alder,
-         speUd_21  = work_b_k1,
-         diplUd_21 = work_b_k2,
-         dayWork_21 = work_schedule_a_m_chc,
-         nightWork_21 = work_schedule_b_m_chc,
-         eveWork_21 = work_schedule_c_m_chc,
-         dayHrs_21 = day_hours_m_chc,
-         nightHrs_21 = evening_hours_m_chc,
-         eveHrs_21 = night_hours_m_chc,
-         mixedHrs_21 = shift_hours_m_chc,
+         #shift frequency / wk 
          nightFreq_24 = eve,
          eveFreq_24 = night,
          weekendFreq_24 = weekend,
-         dayWork_24 = day_hours_k_v2,
-         nightWork_24 = evening_hours_k_v2,
-         eveWork_24 = night_hours_k_v2,
-         mixedWork_24 = shift_hours_k_v2,
-         weekendWork_24 = weekend_k_v2,
-         LGBTID = lbgt,
+         #no. of yrs working a shift type
+         dayScheYrs_24 = day_hours_k_v2,
+         nightScheYrs_24 = evening_hours_k_v2,
+         eveScheYrs_24 = night_hours_k_v2,
+         lgbtID = lbgt,
          famInh_24 = inheritage_icd_v2,
          obeInh_24 = inheritage_icd_v3___5
   )
 
-##treat age----
+
 ds <- ds %>%
   mutate(age_2021 = trunc(age_2021),
          age_2024 = trunc(age_2024))
-
 ds <- ds %>%
   mutate(
     age_2021_imputed = if_else(
       is.na(age_2021) & !is.na(age_2024),
-      age_2024-3,
-      age_2021
-    )
+      trunc(age_2024)-3,
+      trunc(age_2021)
   )
-
-ds <- ds %>%
-  mutate(
-    age_2021_imputed=trunc(age_2021_imputed)
   )
-
-ds <- ds %>%
-  mutate(
-    age_2024_imputed = if_else(
-      !is.na(age_2021) & is.na(age_2024),
-      age_2021+3,
-      age_2024
-    )
-  )
-
 ds <- ds %>%
   mutate(
     age_flag = if_else(
@@ -111,22 +85,198 @@ ds <- ds %>%
     )
   )
 
-##age 25+----
 ds <- ds %>%
   filter(age_2021_imputed >= 25)
 
-# Full distribution of LGBTID
-table(ds$LGBTID, useNA = "always")
+ds <- ds %>%
+  filter(cpr_sex==1)
 
-# Cross tabulation with sex
-table(ds$cpr_sex, ds$LGBTID, useNA = "always")
 
-##making height to integer ----
-v3 <- v3 %>%
-  mutate(height_2021 = trunc(height_2021),
-         height_2024 = trunc(height_2024))
 
-##making weight to integer ----
-v3 <- v3 %>%
-  mutate(weight_2021 = trunc(weight_2021),
-         weight_2024 = trunc(weight_2024))
+
+#WEIGHT----
+##weight rescue----
+library(dplyr)
+library(gtsummary)
+
+rescue_W21 <- function(w_input, w_anchor) {
+  x <- if_else(is.na(w_input) | w_input == 0, w_anchor, w_input)
+  x <- if_else(!is.na(x) & x < 0, abs(x), x)
+  case_when(
+    x > 0  & x < 1   ~ x * 100,
+    x >= 2 & x < 10  ~ x * 10,
+    x >= 10 & x < 40 ~ w_anchor,
+    x >= 40 & x <= 190 ~ x,
+    x > 190 & x < 200 ~ NA_real_,
+    x >= 200  & x < 1500  ~ x / 10,
+    x >= 1500 & x < 10000 ~ x / 100,
+    x >= 10000             ~ x / 1000,
+    TRUE ~ NA_real_
+  )
+}
+
+rescue_W24 <- function(w_input, w_anchor) {
+  x <- if_else(is.na(w_input) | w_input == 0, w_anchor, w_input)
+  case_when(
+    x > 0  & x <= 1   ~ x * 100,
+    x >= 2 & x <= 10  ~ x * 10,
+    x > 10 & x < 40   ~ w_anchor,
+    x >= 40 & x <= 190 ~ x,
+    x > 190 & x <= 1000  ~ x / 10,
+    x > 1000 & x <= 10000 ~ x / 100,
+    x > 10000              ~ x / 1000,
+    TRUE ~ NA_real_
+  )
+}
+
+ds <- ds %>%
+  mutate(
+    TW21_flag = case_when(
+      is.na(W21) & is.na(W24)         ~ NA_character_,
+      is.na(W21) | W21 == 0           ~ "from_W24",
+      W21 < 0                         ~ "negative_flipped",
+      W21 > 0  & W21 < 1              ~ "decimal_x100",
+      W21 >= 2 & W21 < 10             ~ "single_digit_x10",
+      W21 >= 10 & W21 < 40            ~ "from_W24",
+      W21 >= 40 & W21 <= 190          ~ "kept",
+      W21 > 190 & W21 < 200           ~ "gap_NA",
+      W21 >= 200 & W21 < 1500         ~ "div10",
+      W21 >= 1500 & W21 < 10000       ~ "div100",
+      W21 >= 10000                    ~ "div1000",
+      TRUE                            ~ "uncategorised"
+    ),
+    
+    TW24_flag = case_when(
+      is.na(W24) & is.na(W21)         ~ NA_character_,
+      is.na(W24) & !is.na(W21)        ~ "from_TW21",
+      W24 == 0                        ~ "from_TW21",
+      W24 > 0  & W24 <= 1            ~ "decimal_x100",
+      W24 >= 2 & W24 <= 10           ~ "single_digit_x10",
+      W24 > 10 & W24 < 40            ~ "from_TW21",
+      W24 >= 40 & W24 <= 190         ~ "kept",
+      W24 > 190 & W24 <= 1000        ~ "div10",
+      W24 > 1000 & W24 <= 10000      ~ "div100",
+      W24 > 10000                    ~ "div1000",
+      TRUE                           ~ "uncategorised"
+    ),
+    
+    treatedW21 = trunc(rescue_W21(w_input = W21, w_anchor = W24)),
+    treatedW24 = trunc(rescue_W24(w_input = W24, w_anchor = treatedW21)),
+    
+    TW21_flag = if_else(!is.na(treatedW21) & (treatedW21 < 40 | treatedW21 > 190), "out_of_range", TW21_flag),
+    TW24_flag = if_else(!is.na(treatedW24) & (treatedW24 < 40 | treatedW24 > 190), "out_of_range", TW24_flag)
+  )
+##SMD analysis, W21----
+library(smd)
+
+smd_w21 <- smd(
+  x = c(ds$W21, ds$treatedW21),
+  g = c(rep("Raw", nrow(ds)), rep("Treated", nrow(ds))),
+  na.rm = TRUE
+)
+
+print(smd_w21)
+
+data.frame(
+  Raw     = unclass(summary(ds$H21)),
+  Treated = unclass(summary(ds$treatedH21))
+)
+
+##SMD analysis, W24----
+smd_w24 <- smd(
+  x = c(ds$W24, ds$treatedW24),
+  g = c(rep("Raw", nrow(ds)), rep("Treated", nrow(ds))),
+  na.rm = TRUE
+)
+
+print(smd_w24)
+
+#HEIGHT----
+height <- ds %>%
+  select(H21, H24)
+View(height)
+
+rescue_H21 <- function(h_input, h_anchor) {
+  
+  x <- if_else(is.na(h_input) | h_input == 0, h_anchor, h_input)
+  
+  case_when(
+    x <= 1 ~ h_anchor,
+    x > 1 & x < 2  ~ x * 100,
+    x >= 2 & x <= 13 ~ h_anchor,
+    x > 13 & x <= 20  ~ x * 10,
+    x >= 20 & x < 60 ~ h_anchor,
+    x >= 60 & x < 100 ~ x + 100,
+    x >= 100 & x < 140 ~ h_anchor,
+    x >= 140 & x <= 210 ~ x,
+    x > 210  & x <= 1000 ~ (x %% 100) + 100,
+    x > 1000 ~ as.numeric(substr(as.character(x), 1, 3)),
+    TRUE ~ NA_real_
+  )
+}
+
+ds <- ds %>%
+  mutate(
+    TH21_flag = case_when(
+      is.na(H21) & is.na(H24) ~ NA_character_,
+      is.na(H21) | H21 == 0 ~ "from_H24",
+      H21 == 1 ~ "from_H24",
+      H21 > 1   & H21 < 2 ~ "decimal_x100",
+      H21 >= 2  & H21 <= 13 ~ "from_H24",
+      H21 > 13  & H21 <= 20 ~ "teens_x10",
+      H21 >= 20 & H21 < 60 ~ "from_H24",
+      H21 >= 60 & H21 < 100 ~ "missing_leading1",
+      H21 >= 100 & H21 < 140 ~ "from_H24",
+      H21 >= 140 & H21 <= 210 ~ "kept",
+      H21 > 210  & H21 <= 1000 ~ "leading_digit_swap",
+      H21 > 1000                       ~ "leftmost3",
+      TRUE ~ "NA"
+    ),
+    treatedH21 = trunc(rescue_H21(h_input = H21, h_anchor = H24)),
+    TH21_flag = if_else(
+      !is.na(treatedH21) & (treatedH21 < 140 | treatedH21 > 210),
+      "out_of_range",
+      TH21_flag
+    )
+  )
+
+##SMD analysis----
+library(smd)
+
+smd_h21 <- smd(
+  x = c(ds$H21, ds$treatedH21),
+  g = c(rep("Raw", nrow(ds)), rep("Treated", nrow(ds))),
+  na.rm = TRUE
+)
+
+print(smd_h21)
+
+data.frame(
+  Raw     = unclass(summary(ds$H21)),
+  Treated = unclass(summary(ds$treatedH21))
+)
+
+
+
+
+
+
+
+
+#BMI----
+ds <- ds %>%
+  mutate(
+    BMI_21 = if_else(
+      treatedW21 >= 40 & treatedW21 <= 190 & treatedH21 >= 140 & treatedH21 <= 210,
+      treatedW21 / ((treatedH21 / 100)^2),
+      NA_real_
+    ),
+    BMI_24 = if_else(
+      treatedW24 >= 40 & treatedW24 <= 190 & treatedH21 >= 140 & treatedH21 <= 210,
+      treatedW24 / ((treatedH21 / 100)^2),
+      NA_real_
+    )
+  )
+
+
+sum(!is.na(ds$BMI_21) & !is.na(ds$BMI_24) & !is.na(ds$LS21) & !is.na(ds$LS24) & !is.na(ds$WS_a24) & !is.na(ds$WS_d21) & !is.na(ds$WS_d24) & !is.na(ds$WS_a21) & !is.na(ds$momPhys_21) & !is.na(ds$dadPhys21) & !is.na(ds$momPhys_24) & !is.na(ds$dadPhys24))
